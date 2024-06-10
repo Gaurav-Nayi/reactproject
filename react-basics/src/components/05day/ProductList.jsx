ffa49d65fe9654e8c4c4bbc1d475702bad77d34d
Gaurav <gauravnayi1994@gmail.com>
2024-06-10 18:08:01 +0530

    hello

--- react-basics/src/components/05day/ProductList.jsx
+++ react-basics/src/components/05day/ProductList.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import Products from '../products.js'
+import Products from './products.js'
 
 const ProductList = () => {
     console.log(Products)
   return (
-    <div>
-      
-    </div>
+    <>
+      {Products.map((product,index)=><p key={product.id}>
+        {JSON.stringify(product)}</p>)}
+    </>
   )
 }
 
--- react-basics/src/components/05day/products.js
+++ react-basics/src/components/05day/products.js
@@ -9,8 +9,8 @@ import Image7 from '../../assets/1.jpg'
 let Products = [
     {id:"1", name:"product1",price:2000,category:'grocery',brand:'brand1',image:Image1},
     {id:"2", name:"product2",price:4000,category:'electronics',brand:'brand2',image:Image2},
-    {id:"3", name:"product3",price:6000,category:'accessories',brand:'brand3',image:Image4},
-    {id:"4", name:"product4",price:7000,category:'fashion',brand:'brand4',image:Image3},
+    {id:"3", name:"product3",price:6000,category:'accessories',brand:'brand3',image:Image3},
+    {id:"4", name:"product4",price:7000,category:'fashion',brand:'brand4',image:Image4},
     {id:"5",name:"product5",price:9000,category:'electronics',brand:'brand1',image:Image5},
     {id:"6", name:"product6",price:10000,category:'grocery',brand:'brand5',image:Image6},
     {id:"7", name:"product7",price:2700,category:'grocery',brand:'brand3',image:Image7}
