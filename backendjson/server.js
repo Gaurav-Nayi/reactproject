4546d6e1a42759635961757deb7537dfff9e394a
Gaurav <gauravnayi1994@gmail.com>
2024-07-18 14:49:16 +0530

    g

--- backendjson/server.js
+++ backendjson/server.js
@@ -3,8 +3,8 @@ import fs from 'fs'
 import path from 'path'
 const server = jsonServer.create()
 const __dirname = path.resolve();
-const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'data.json')))
-const router = jsonServer.router(db)
+// const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'data.json')))
+const router = jsonServer.router(path.join(__dirname, 'data.json'))
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
